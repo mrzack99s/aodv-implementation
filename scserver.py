95e67ed190ead88f4dd02cc4221d14913cd01671
Chatdanai Phakaket <zchatdanai@gmail.com>
2020-03-31 23:56:00 +0700

    issue-4 Add check expire function - close #4

--- scserver.py
+++ scserver.py
@@ -1,17 +1,61 @@
 import json
 import socket
 import threading
+import time
 from datetime import datetime
+
+
+def A(routing_table):
+    idleTime = {}
+    while True:
+        if routing_table:
+            for route in routing_table:
+                toDatetime = datetime.fromtimestamp(route["Lastest_used"])
+                diffTime = datetime.now() - toDatetime
+
+                try:
+                    idleTime[route["ID"]] = divmod(diffTime.seconds, 60)[0]
+                except:
+                    idleTime.update({
+                        route["ID"]:divmod(diffTime.seconds, 60)[0]
+                    })
+
+                if idleTime[route["ID"]] >= route["Lifetime"]:
+                    print("Route ID ",route["ID"],"is expire")
+                    routing_table.pop(routing_table.index(route))
+
+                # if checkingTime >= routing_table["Lifetime"]:
+                #     print("Expire")
+            #print(datetime.now().second - toDatetime.second)
+
+        time.sleep(10)
+    # while True:
+    #     if routing_table:
+    #         divv = datetime.fromtimestamp(routing_table["Lastest_used"])
+    #         print(divv.second - routing_table["Lifetime"] *60)
+    #         time.sleep(5)
 
 
 class UDPSocketServer(threading.Thread):
 
     def __init__(self, listNode, nodeName):
+
         super().__init__()
         self.listNode = listNode
         self.node = self.listNode[nodeName]
         self.rreq_data_packet = None
         self.rrep_data_packet = None
+        self.trickFirstRouting = False
+
+    def getNode(self):
+        return
+
+    def runExpireTimer(self):
+        print("################## Timer at "+self.node["Name"]+" Start ###############")
+        self.trickFirstRouting = True
+        t = threading.Thread(target=A, args=(self.node["ROUTING_TABLE"],))
+        t.setDaemon(True)
+        t.start()
 
     def run(self):
 
@@ -19,7 +63,10 @@ class UDPSocketServer(threading.Thread):
         s.bind((self.node["IP"], self.node["Port"]))
         s.listen(5)
 
+        self.runExpireTimer()
+
         while True:
+
             # now our endpoint knows about the OTHER endpoint.
             clientsocket, address = s.accept()
             if clientsocket:
@@ -27,44 +74,10 @@ class UDPSocketServer(threading.Thread):
 
                 revMessage = json.loads(clientsocket.recv(1024).decode())
                 if revMessage["isRREQ"] and revMessage["sourceAddr"] != self.node["Name"]:
-
-                    self.rreq_data_packet = revMessage
-                    self.rreq_data_packet["hopCount"] += 1
-                    self.rreq_data_packet["RequestTime"].update({
-                        self.node["Name"]: datetime.timestamp(datetime.now())
-                    })
-
-                    rreq_message = {
-                        "destAddr": self.rreq_data_packet["sourceAddr"],
-                        "nextHop": self.rreq_data_packet["sentFrom"],
-                        "hopCount": self.rreq_data_packet["hopCount"],
-                        "seq": self.rreq_data_packet["seqSource"]
-                    }
-                    self.node["RREQ_MESSAGE"].append(rreq_message)
-                    self.rreq_data_packet["sentFrom"] = self.node["Name"]
+                    self.aodvRREQ(revMessage)
 
                 elif not revMessage["isRREQ"] and revMessage["sourceAddr"] != self.node["Name"]:
-
-                    self.rrep_data_packet = revMessage
-                    self.rrep_data_packet["hopCount"] += 1
-
-                    rrep_message = {
-                        "destAddr": self.rrep_data_packet["sourceAddr"],
-                        "nextHop": self.rrep_data_packet["sentFrom"],
-                        "hopCount": self.rrep_data_packet["hopCount"],
-                        "seq": self.rrep_data_packet["destSeq"]
-                    }
-
-                    RREP_Time = datetime.now()
-                    duration = RREP_Time - datetime.fromtimestamp(
-                        self.rreq_data_packet["RequestTime"].pop(self.node["Name"]))
-                    Latency_Time = duration.microseconds * (10 ** -3)
-                    rrep_message.update({
-                        "LatencyTime": str(Latency_Time) + " ms"
-                    })
-
-                    self.node["ROUTING_TABLE"].append(rrep_message)
-                    self.rrep_data_packet["sentFrom"] = self.node["Name"]
+                    self.aodvRREP(revMessage)
 
                 if self.rrep_data_packet is None and revMessage["sourceAddr"] != self.node["Name"]:
                     if self.node["Name"] != self.rreq_data_packet["destAddr"] and self.node["Name"] != \
@@ -82,10 +95,12 @@ class UDPSocketServer(threading.Thread):
                             "destSeq": 120,
                             "hopCount": 0,
                             "sentFrom": self.node["Name"],
-                            "isRREQ": False
+                            "isRREQ": False,
+                            "Lifetime": 1  # if 0 is not expire || unit is min
                         }
 
                         self.__sendRequest(neighborName=self.node["RREQ_MESSAGE"][-1]["nextHop"], isRREQ=False)
+
 
                 elif self.rrep_data_packet is not None and revMessage["sourceAddr"] != self.node["Name"]:
                     if self.node["Name"] != self.rrep_data_packet["destAddr"] and self.node["Name"] != \
@@ -104,6 +119,7 @@ class UDPSocketServer(threading.Thread):
                             self.__sendRequest(neighborName=neighbor, isRREQ=True)
 
                 clientsocket.close()
+
 
     def __sendRequest(self, neighborName=None, isRREQ=True):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as ss:
@@ -138,3 +154,49 @@ class UDPSocketServer(threading.Thread):
         for reccord in self.node["ROUTING_TABLE"]:
             print("Routing table of node " + self.node["Name"])
             print(reccord, end="\n\n")
+
+    def aodvRREQ(self, revMessage):
+
+        self.rreq_data_packet = revMessage
+        self.rreq_data_packet["hopCount"] += 1
+        self.rreq_data_packet["RequestTime"].update({
+            self.node["Name"]: datetime.timestamp(datetime.now())
+        })
+
+        rreq_message = {
+            "destAddr": self.rreq_data_packet["sourceAddr"],
+            "nextHop": self.rreq_data_packet["sentFrom"],
+            "hopCount": self.rreq_data_packet["hopCount"],
+            "seq": self.rreq_data_packet["seqSource"]
+        }
+        self.node["RREQ_MESSAGE"].append(rreq_message)
+        self.rreq_data_packet["sentFrom"] = self.node["Name"]
+
+    def aodvRREP(self, revMessage):
+
+        self.rrep_data_packet = revMessage
+        self.rrep_data_packet["hopCount"] += 1
+
+        rrep_message = {
+            "ID":len(self.node["ROUTING_TABLE"])+1,
+            "destAddr": self.rrep_data_packet["sourceAddr"],
+            "nextHop": self.rrep_data_packet["sentFrom"],
+            "hopCount": self.rrep_data_packet["hopCount"],
+            "seq": self.rrep_data_packet["destSeq"]
+        }
+
+        RREP_Time = datetime.now()
+        duration = RREP_Time - datetime.fromtimestamp(
+            self.rreq_data_packet["RequestTime"][self.node["Name"]]
+        )
+        Latency_Time = duration.microseconds * (10 ** -3)
+        rrep_message.update({
+            "LatencyTime": str(Latency_Time) + " ms",
+            "Lifetime": self.rrep_data_packet["Lifetime"],
+            "Lastest_used": datetime.timestamp(datetime.now())
+        })
+
+        self.node["ROUTING_TABLE"].append(rrep_message)
+        self.rrep_data_packet["sentFrom"] = self.node["Name"]
+
+
--- test.py
+++ test.py
@@ -5,55 +5,61 @@ import scserver
 listNode = {}
 
 nodeC = {
-            "ID":3,
-            "Name": "nodeC",
-            "IP": "127.0.0.1",
-            "Port":1236,
-            "neighbors": ["nodeB"],
-            "RREQ_MESSAGE": [],
-            "ROUTING_TABLE": [],
-        }
+    "ID": 3,
+    "Name": "nodeC",
+    "IP": "127.0.0.1",
+    "Port": 1236,
+    "neighbors": ["nodeB"],
+    "RREQ_MESSAGE": [],
+    "ROUTING_TABLE": [],
+}
 
 nodeB = {
-            "ID":2,
-            "Name": "nodeB",
-            "IP": "127.0.0.1",
-            "Port": 1235,
-            "neighbors": ["nodeA", "nodeC"],
-            "RREQ_MESSAGE": [],
-            "ROUTING_TABLE": []
-        }
+    "ID": 2,
+    "Name": "nodeB",
+    "IP": "127.0.0.1",
+    "Port": 1235,
+    "neighbors": ["nodeA", "nodeC"],
+    "RREQ_MESSAGE": [],
+    "ROUTING_TABLE": []
+}
 
 nodeA = {
-            "ID": 1,
-            "Name": "nodeA",
-            "IP": "127.0.0.1",
-            "Port": 1234,
-            "neighbors": ["nodeB"],
-            "RREQ_MESSAGE": [],
-            "ROUTING_TABLE": []
-        }
+    "ID": 1,
+    "Name": "nodeA",
+    "IP": "127.0.0.1",
+    "Port": 1234,
+    "neighbors": ["nodeB"],
+    "RREQ_MESSAGE": [],
+    "ROUTING_TABLE": []
+}
 
 listNode.update({
-            "nodeA": nodeA,
-            "nodeB": nodeB,
-            "nodeC": nodeC
-        })
+    "nodeA": nodeA,
+    "nodeB": nodeB,
+    "nodeC": nodeC
+})
 
-udpServer = scserver.UDPSocketServer(listNode,'nodeA')
-udpServer1 = scserver.UDPSocketServer(listNode,'nodeB')
-udpServer2 = scserver.UDPSocketServer(listNode,'nodeC')
+udpServer = scserver.UDPSocketServer(listNode, 'nodeA')
+udpServer1 = scserver.UDPSocketServer(listNode, 'nodeB')
+udpServer2 = scserver.UDPSocketServer(listNode, 'nodeC')
 
 if __name__ == "__main__":
     udpServer.start()
     udpServer1.start()
     udpServer2.start()
+
     udpServer.requestDiscoveryPath(toNode="nodeC")
+    udpServer2.requestDiscoveryPath(toNode="nodeA")
     time.sleep(0.5)
     udpServer.getRoutingTable()
     udpServer1.getRoutingTable()
     udpServer2.getRoutingTable()
-    #print(json.dumps(listNode, indent=2))
 
+    print("\n\n\n\n")
+    time.sleep(62)
+    udpServer.getRoutingTable()
+    udpServer1.getRoutingTable()
+    udpServer2.getRoutingTable()
 
-
+    # print(json.dumps(listNode, indent=2))
