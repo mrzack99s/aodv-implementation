a6ac6fdbc5b9b9e647dac1b242d8b52b934f87bf
Chatdanai Phakaket <zchatdanai@gmail.com>
2020-04-04 21:51:33 +0700

    Move from nodeName for discovery path  to IPv6 and move list node to json file with append API

--- scserver.py
+++ scserver.py
@@ -74,55 +74,56 @@ class UDPSocketServer(threading.Thread):
                 revMessage = revMessage.decode()
                 revMessage = json.loads(revMessage)
 
-                if revMessage["isRREQ"] and revMessage["sourceAddr"] != self.node["Name"]:
+
+                if revMessage["isRREQ"] and revMessage["sourceAddr"] != self.node["IP"]:
                     self.aodvRREQ(revMessage)
 
-                elif not revMessage["isRREQ"] and revMessage["sourceAddr"] != self.node["Name"]:
+                elif not revMessage["isRREQ"] and revMessage["sourceAddr"] != self.node["IP"]:
                     self.aodvRREP(revMessage)
 
-                if self.rrep_data_packet is None and revMessage["sourceAddr"] != self.node["Name"]:
-                    if self.node["Name"] != self.rreq_data_packet["destAddr"] and self.node["Name"] != \
+                if self.rrep_data_packet is None and revMessage["sourceAddr"] != self.node["IP"]:
+                    if self.node["IP"] != self.rreq_data_packet["destAddr"] and self.node["IP"] != \
                             self.rreq_data_packet["sourceAddr"]:
                         # print(self.node["Name"])
                         for neighbor in self.node["neighbors"]:
                             if neighbor != self.rreq_data_packet["sourceAddr"]:
                                 # print(neighbor)
-                                self.__sendRequest(neighborName=neighbor, isRREQ=True)
+                                self.__sendRequest(neighborIP=neighbor, isRREQ=True)
 
-                    elif self.node["Name"] == self.rreq_data_packet["destAddr"]:
+                    elif self.node["IP"] == self.rreq_data_packet["destAddr"]:
                         self.rrep_data_packet = {
                             "sourceAddr": self.rreq_data_packet["destAddr"],
                             "destAddr": self.rreq_data_packet["sourceAddr"],
                             "destSeq": 120,
                             "hopCount": 0,
-                            "sentFrom": self.node["Name"],
+                            "sentFrom": self.node["IP"],
                             "isRREQ": False,
                             "Lifetime": 1  # if 0 is not expire || unit is min
                         }
 
-                        self.__sendRequest(neighborName=self.node["RREQ_MESSAGE"][-1]["nextHop"], isRREQ=False)
+                        self.__sendRequest(neighborIP=self.node["RREQ_MESSAGE"][-1]["nextHop"], isRREQ=False)
 
 
-                elif self.rrep_data_packet is not None and revMessage["sourceAddr"] != self.node["Name"]:
-                    if self.node["Name"] != self.rrep_data_packet["destAddr"] and self.node["Name"] != \
+                elif self.rrep_data_packet is not None and revMessage["sourceAddr"] != self.node["IP"]:
+                    if self.node["IP"] != self.rrep_data_packet["destAddr"] and self.node["IP"] != \
                             self.rrep_data_packet["sourceAddr"]:
                         # print(self.node["Name"])
                         for neighbor in self.node["neighbors"]:
                             if neighbor != self.rrep_data_packet["sourceAddr"]:
                                 # print(neighbor)
-                                self.__sendRequest(neighborName=neighbor, isRREQ=False)
+                                self.__sendRequest(neighborIP=neighbor, isRREQ=False)
                 else:
                     self.rreq_data_packet = revMessage
 
                     for neighbor in self.node["neighbors"]:
                         if neighbor != self.rreq_data_packet["sourceAddr"]:
                             # print(neighbor)
-                            self.__sendRequest(neighborName=neighbor, isRREQ=True)
+                            self.__sendRequest(neighborIP=neighbor, isRREQ=True)
 
 
 
-    def __sendRequest(self, neighborName=None, isRREQ=True):
-        sockAddr = socket.getaddrinfo(self.listNode[neighborName]["IP"], self.listNode[neighborName]["Port"], family=socket.AF_INET6, proto=socket.IPPROTO_UDP)
+    def __sendRequest(self, neighborIP=None, isRREQ=True):
+        sockAddr = socket.getaddrinfo(self.listNode[neighborIP]["IP"], self.listNode[neighborIP]["Port"], family=socket.AF_INET6, proto=socket.IPPROTO_UDP)
         with socket.socket(family=socket.AF_INET6, type=socket.SOCK_DGRAM) as ss:
             if isRREQ:
                 ss.sendto(json.dumps(self.rreq_data_packet).encode(),sockAddr[0][4])
@@ -130,30 +131,30 @@ class UDPSocketServer(threading.Thread):
                 ss.sendto(json.dumps(self.rrep_data_packet).encode(),sockAddr[0][4])
             ss.close()
 
-    def requestDiscoveryPath(self, toNode=None):
+    def requestDiscoveryPath(self, toNodeIP=None):
         sockAddr = socket.getaddrinfo(self.node["IP"], self.node["Port"], family=socket.AF_INET6, proto=socket.IPPROTO_UDP)
 
         with socket.socket(family=socket.AF_INET6, type=socket.SOCK_DGRAM) as ss:
             rreq_data_packet = {
-                "sourceAddr": self.node["Name"],
+                "sourceAddr": self.node["IP"],
                 "seqSource": 1,
                 "RreqId": self.node["ID"],
-                "destAddr": toNode,
+                "destAddr": toNodeIP,
                 "destSeq": None,
                 "hopCount": 0,
-                "sentFrom": self.node["Name"],
+                "sentFrom": self.node["IP"],
                 "isRREQ": True,
                 "RequestTime": {}
             }
             rreq_data_packet["RequestTime"].update({
-                self.node["Name"]: datetime.timestamp(datetime.now())
+                self.node["IP"]: datetime.timestamp(datetime.now())
             })
             ss.sendto(json.dumps(rreq_data_packet).encode(),sockAddr[0][4])
             ss.close()
 
     def getRoutingTable(self):
         for reccord in self.node["ROUTING_TABLE"]:
-            print("Routing table of node " + self.node["Name"])
+            print("Routing table of node " + self.node["IP"])
             print(reccord, end="\n\n")
 
     def aodvRREQ(self, revMessage):
@@ -161,7 +162,7 @@ class UDPSocketServer(threading.Thread):
         self.rreq_data_packet = revMessage
         self.rreq_data_packet["hopCount"] += 1
         self.rreq_data_packet["RequestTime"].update({
-            self.node["Name"]: datetime.timestamp(datetime.now())
+            self.node["IP"]: datetime.timestamp(datetime.now())
         })
 
         rreq_message = {
@@ -171,7 +172,7 @@ class UDPSocketServer(threading.Thread):
             "seq": self.rreq_data_packet["seqSource"]
         }
         self.node["RREQ_MESSAGE"].append(rreq_message)
-        self.rreq_data_packet["sentFrom"] = self.node["Name"]
+        self.rreq_data_packet["sentFrom"] = self.node["IP"]
 
     def aodvRREP(self, revMessage):
 
@@ -188,7 +189,7 @@ class UDPSocketServer(threading.Thread):
 
         RREP_Time = datetime.now()
         duration = RREP_Time - datetime.fromtimestamp(
-            self.rreq_data_packet["RequestTime"][self.node["Name"]]
+            self.rreq_data_packet["RequestTime"][self.node["IP"]]
         )
         Latency_Time = duration.microseconds * (10 ** -3)
         rrep_message.update({
@@ -198,6 +199,6 @@ class UDPSocketServer(threading.Thread):
         })
 
         self.node["ROUTING_TABLE"].append(rrep_message)
-        self.rrep_data_packet["sentFrom"] = self.node["Name"]
+        self.rrep_data_packet["sentFrom"] = self.node["IP"]
 
 
